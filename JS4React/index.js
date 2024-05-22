a286f50438be0ddd8319d58722ffcc53b31abb60
RomanFF123 <121040623@upq.edu.mx>
2024-05-22 14:58:05 -0600

    Practica 2 Terminada
    Trabajo con las propiedades de los objetos, metodos y trabajo de DOM

--- JS4React/index.js
+++ JS4React/index.js
@@ -19,5 +19,28 @@ const usuario={
     },
     amigos: ['Saimon', 'Luis'],
     estatus: true,
-
+    enviaCorreo: enviarCorr=()=>'enviando...'
 }
+
+//Propiedades
+//console.log(usuario);
+//console.log(usuario.amigos);
+//console.log(usuario.direccion);
+
+//Metodos
+//console.log(usuario.enviaCorreo())
+
+// TRABAJO CON DOM 
+const titulo = document.createElement('h1')
+titulo.innerText = 'Titulo creado con JavaScript'
+document.body.append(titulo) 
+
+const boton = document.createElement('button')
+boton.innerText = 'Presioname'
+document.body.append(boton)
+
+boton.addEventListener('click', function(){
+    //console.log('Evento Click ejecutado')
+    titulo.innerText="Evento Click ejecutado"
+    // alert('exito')
+})
